ed291b05a1893fca804dc15b94bf3503167702e2
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-18 17:59:12 -0300

    TUTORIAL SELECT WORLD requisitos 1 a 9

--- bloco 20/20.2/exercicio-bonus-SELECT-from-WORLD.sql
+++ bloco 20/20.2/exercicio-bonus-SELECT-from-WORLD.sql
@@ -18,3 +18,8 @@ WHERE name LIKE '%United%'
 -- Show the countries that are big by area or big by population. Show name, population and area.
 SELECT name, population, area FROM world
 WHERE area > 3000000 OR population > 250000000
+-- Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+-- For South America show population in millions and GDP in billions both to 2 decimal places.
+-- Divide by 1000000 (6 zeros) for millions. Divide by 1000000000 (9 zeros) for billions.
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world
+WHERE continent = 'South America'
