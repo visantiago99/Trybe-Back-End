f39f37be82c440cf4b965d1ca127104295a7fb15
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-19 14:55:54 -0300

    Exercise fixation req 4

--- bloco 20/20.3/exercise-fixation-20-3.sql
+++ bloco 20/20.3/exercise-fixation-20-3.sql
@@ -9,5 +9,7 @@ SELECT title, description, release_year, replacement_cost FROM sakila.film
 WHERE rating <> 'NC-17'
 ORDER BY replacement_cost DESC, title LIMIT 100;
 -- Quantos clientes estão ativos e na loja 1 ?
+SELECT COUNT(first_name) FROM sakila.customer
+WHERE active = 1 AND store_id = 1;
 -- Mostre todos os detalhes dos clientes que não estão ativos na loja 1 .
 -- Precisamos descobrir quais são os 50 filmes feitos apenas para adultos com a menor taxa de aluguel, para que possamos fazer uma divulgação melhor desses filmes. Em caso de empate, ordene em ordem alfabética pelo título.
