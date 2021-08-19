2e666b9bc1b0d25884384ba41df56a3fefeec9c3
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-19 14:43:57 -0300

    Exercise fixation req 2

--- bloco 20/20.3/exercise-fixation-20-3.sql
+++ bloco 20/20.3/exercise-fixation-20-3.sql
@@ -2,6 +2,8 @@
 SELECT * FROM sakila.customer
 WHERE email = 'LEONARD.SCHOFIELD@sakilacustomer.org';
 -- Precisamos de um relatório dos nomes dos clientes, em ordem alfabética , que não estão mais ativos no nosso sistema e pertencem à loja com o id = 2 , e não inclua o cliente KENNETH no resultado.
+SELECT first_name FROM sakila.customer
+WHERE (active = 0 AND store_id = 2) AND first_name <> 'KENNETH';
 -- O setor financeiro quer saber título, descrição, ano de lançamento e valor do custo de substituição ( replacement_cost ), dos 100 filmes com o maior custo de substituição, do valor mais alto ao mais baixo, entre os filmes feitos para menores de idade e que têm o custo mínimo de substituição de $18,00 dólares. Em caso de empate, ordene em ordem alfabética pelo título.
 -- Quantos clientes estão ativos e na loja 1 ?
 -- Mostre todos os detalhes dos clientes que não estão ativos na loja 1 .
