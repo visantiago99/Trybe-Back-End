ec4d14495e9924baa7ad72aefdd63645ca605968
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-19 14:52:58 -0300

    Exercise fixation req 3(cool one)

--- bloco 20/20.3/exercise-fixation-20-3.sql
+++ bloco 20/20.3/exercise-fixation-20-3.sql
@@ -5,6 +5,9 @@ WHERE email = 'LEONARD.SCHOFIELD@sakilacustomer.org';
 SELECT first_name FROM sakila.customer
 WHERE (active = 0 AND store_id = 2) AND first_name <> 'KENNETH';
 -- O setor financeiro quer saber título, descrição, ano de lançamento e valor do custo de substituição ( replacement_cost ), dos 100 filmes com o maior custo de substituição, do valor mais alto ao mais baixo, entre os filmes feitos para menores de idade e que têm o custo mínimo de substituição de $18,00 dólares. Em caso de empate, ordene em ordem alfabética pelo título.
+SELECT title, description, release_year, replacement_cost FROM sakila.film
+WHERE rating <> 'NC-17'
+ORDER BY replacement_cost DESC, title LIMIT 100;
 -- Quantos clientes estão ativos e na loja 1 ?
 -- Mostre todos os detalhes dos clientes que não estão ativos na loja 1 .
 -- Precisamos descobrir quais são os 50 filmes feitos apenas para adultos com a menor taxa de aluguel, para que possamos fazer uma divulgação melhor desses filmes. Em caso de empate, ordene em ordem alfabética pelo título.
