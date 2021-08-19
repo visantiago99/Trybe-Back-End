77b9f8d62be3f0f76883be527090f9ddda5cb507
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-19 15:52:36 -0300

    Exercise fixation 2, req 1

---
+++ bloco 20/20.3/exercise-fixation-2-20-3.sql
@@ -0,0 +1,9 @@
+-- Mostre todos os detalhes dos filmes que contêm a palavra ace no nome.
+SELECT * FROM sakila.film
+WHERE title LIKE '%ace%';
+-- Mostre todos os detalhes dos filmes cujas descrições finalizam com china .
+-- Mostre todos os detalhes dos dois filmes cujas descrições contêm a palavra girl e o título finaliza com a palavra lord .
+-- Mostre os dois casos em que, a partir do 4° caractere no título do filme, tem-se a palavra gon .
+-- Mostre o único caso em que, a partir do 4° caractere no título do filme, tem-se a palavra gon e a descrição contém a palavra Documentary .
+-- Mostre os dois filmes cujos títulos ou finalizam com academy ou iniciam com mosquito .
+-- Mostre os seis filmes que contêm as palavras monkey e sumo em suas descrições.
