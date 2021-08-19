7437c2595405d3d200a201cdfecf8904d87c22b6
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-19 16:53:06 -0300

    Exercise bonus 3, req 2

--- bloco 20/20.3/exercise-fixation-3-20-3.sql
+++ bloco 20/20.3/exercise-fixation-3-20-3.sql
@@ -3,6 +3,9 @@ SELECT first_name, last_name, email FROM sakila.customer
 WHERE last_name IN('hicks', 'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy')
 ORDER BY first_name
 -- Mostre o e-mail dos clientes com os address_id 172, 173, 174, 175 e 176 , ordenados em ordem alfabética.
+SELECT email FROM sakila.customer
+WHERE address_id BETWEEN 172 AND 176
+ORDER BY email;
 -- Descubra quantos pagamentos foram feitos entre 01/05/2005 e 01/08/2005 . Lembre-se de que, no banco de dados, as datas estão armazenadas no formato ano/mês/dia , diferente do formato brasileiro, que é dia/mês/ano .
 -- Mostre o título, ano de lançamento e duração do empréstimo de todos os filmes com a duração de empréstimo de 3 a 6 . Os resultados devem ser classificados em filmes com maior duração de empréstimo primeiro. Em caso de empate, ordene em ordem alfabética pelo título.
 -- Monte um relatório que exiba o título e classificação dos 500 primeiros filmes direcionados para as classificações indicativas G, PG e PG-13 . Os resultados devem estar ordenados por título.
