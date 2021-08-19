47348c62ff35f091f858aa29837064d4879b0b33
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-19 15:54:57 -0300

    Exercise bonus 2, req 2

--- bloco 20/20.3/exercise-fixation-2-20-3.sql
+++ bloco 20/20.3/exercise-fixation-2-20-3.sql
@@ -2,6 +2,8 @@
 SELECT * FROM sakila.film
 WHERE title LIKE '%ace%';
 -- Mostre todos os detalhes dos filmes cujas descrições finalizam com china .
+SELECT * FROM sakila.film
+WHERE description LIKE '%china';
 -- Mostre todos os detalhes dos dois filmes cujas descrições contêm a palavra girl e o título finaliza com a palavra lord .
 -- Mostre os dois casos em que, a partir do 4° caractere no título do filme, tem-se a palavra gon .
 -- Mostre o único caso em que, a partir do 4° caractere no título do filme, tem-se a palavra gon e a descrição contém a palavra Documentary .
