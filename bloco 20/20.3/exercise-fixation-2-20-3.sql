8297db34c95b163a944e268e61ab21ea94597dfc
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-19 16:00:21 -0300

    Exercise bonus 2, req 4

--- bloco 20/20.3/exercise-fixation-2-20-3.sql
+++ bloco 20/20.3/exercise-fixation-2-20-3.sql
@@ -8,6 +8,8 @@ WHERE description LIKE '%china';
 SELECT * FROM sakila.film
 WHERE description LIKE '%girl%' AND title LIKE '%lord';
 -- Mostre os dois casos em que, a partir do 4° caractere no título do filme, tem-se a palavra gon .
+SELECT * FROM sakila.film
+WHERE title LIKE '___gon%';
 -- Mostre o único caso em que, a partir do 4° caractere no título do filme, tem-se a palavra gon e a descrição contém a palavra Documentary .
 -- Mostre os dois filmes cujos títulos ou finalizam com academy ou iniciam com mosquito .
 -- Mostre os seis filmes que contêm as palavras monkey e sumo em suas descrições.
