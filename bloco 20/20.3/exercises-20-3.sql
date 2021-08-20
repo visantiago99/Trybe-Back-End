46d6b47dbd9751c3c6d07e6aef0f3186fe808d20
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-20 12:44:43 -0300

    Exercise 20.3, req 3

--- bloco 20/20.3/exercises-20-3.sql
+++ bloco 20/20.3/exercises-20-3.sql
@@ -6,6 +6,8 @@ SELECT * FROM pecasfornecedores.fornecimentos
 WHERE peca = 2
 ORDER BY Fornecedor
 -- Escreva uma query para exibir as peças, preço e fornecedor de todos os fornecimentos em que o código do fornecedor tenha a letra N .
+SELECT peca, Preco, Fornecedor FROM pecasfornecedores.fornecimentos
+WHERE Fornecedor LIKE '%N%'
 -- Escreva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA). Ordene os resultados em ordem alfabética decrescente.
 -- Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código.
 -- Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00 . Ordene os resultados por ordem crescente.
