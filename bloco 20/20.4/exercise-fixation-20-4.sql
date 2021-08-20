a85f12c1fbad98ea4b6d1728c2ae62afe3c6b608
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-20 14:13:30 -0300

    Exercise Fixation 20.4, req 1

---
+++ bloco 20/20.4/exercise-fixation-20-4.sql
@@ -0,0 +1,11 @@
+-- Insira um novo funcionário na tabela sakila.staff .
+-- Para saber quais campos são obrigatórios, clique com o botão direito na tabela sakila.staff e selecione "Table Inspector". Clique na aba "columns" e verifique quais campos aceitam nulos para te guiar. Lembre-se de que valores que são gerados automaticamente não precisam ser inseridos manualmente. Boa explorada!
+INSERT INTO sakila.staff
+(first_name, last_name, address_id, email, store_id, active, username, password)
+VALUES
+('xablau', 'xablozo', 5, 'xablz@gmail.com', 1, 1, 'xau', 'senhaxau'),
+-- Feito o exercício anterior, vamos agora para o nível 2. Insira dois funcionários novos em apenas uma query .
+
+-- Selecione os cinco primeiros nomes e sobrenomes da tabela sakila.customer e cadastre essas pessoas como atores na tabela sakila.actor .
+-- Cadastre três categorias de uma vez só na tabela sakila.category .
+-- Cadastre uma nova loja na tabela sakila.store .
