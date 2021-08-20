ac142491055b3e9ec441df45267f403547a503b0
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-20 12:31:32 -0300

    Exercises BACK END 20.3

---
+++ bloco 20/20.3/exercises-20-3.sql
@@ -0,0 +1,9 @@
+-- Escreva uma query para exibir todas as peças que começam com GR .
+SELECT * FROM pecasfornecedores.pecas
+WHERE name LIKE 'GR%';
+-- Escreva uma query para exibir todos os fornecimentos que contenham a peça com code 2 . Organize o resultado por ordem alfabética de fornecedor.
+-- Escreva uma query para exibir as peças, preço e fornecedor de todos os fornecimentos em que o código do fornecedor tenha a letra N .
+-- Escreva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA). Ordene os resultados em ordem alfabética decrescente.
+-- Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código.
+-- Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00 . Ordene os resultados por ordem crescente.
+-- Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019 .
