c7e3fd86280c92326d623370467b3b47b0b91a2b
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-20 15:32:32 -0300

    Exercise fixation 3, 20.4, req 2

--- bloco 20/20.4/exercise-fixation-3-20-4.sql
+++ bloco 20/20.4/exercise-fixation-3-20-4.sql
@@ -4,6 +4,10 @@ WHERE actor_id = 12;
 DELETE FROM sakila.actor
 WHERE first_name = 'KARL';
 -- Exclua do banco de dados os atores com o nome de "MATTHEW".
+DELETE FROM sakila.film_actor
+WHERE actor_id IN (8, 103, 181);
+DELETE FROM sakila.actor
+WHERE first_name = 'MATTHEW';
 -- Exclua da tabela film_text todos os registros que possuem a palavra "saga" em suas descrições.
 -- Apague da maneira mais performática possível todos os registros das tabelas film_actor e film_category .
 -- Inspecione todas as tabelas do banco de dados sakila e analise quais restrições ON DELETE foram impostas em cada uma. Use o Table Inspector para fazer isso (aba DDL).
