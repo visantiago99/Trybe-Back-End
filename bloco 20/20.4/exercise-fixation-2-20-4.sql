ded974f416f4ffe43bf2bf297350299d0f0605e4
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-20 14:58:32 -0300

    Exercise fixation 2, 20.4, req 3(cool one)

--- bloco 20/20.4/exercise-fixation-2-20-4.sql
+++ bloco 20/20.4/exercise-fixation-2-20-4.sql
@@ -8,4 +8,9 @@ UPDATE sakila.category
 SET name = 'Science Fiction'
 WHERE name = 'Sci-Fi';
 -- Atualize o valor do aluguel para $25 de todos os filmes com duração maior que 100 minutos e que possuem a classificações "G" , "PG" ou "PG-13" e um custo de substituição maior que $20.
+UPDATE sakila.film
+SET rental_rate = 25
+WHERE length > 100
+AND rating IN('G', 'PG', 'PG-13')
+AND replacement_cost > 20;
 -- Foi determinado pelo setor financeiro que haverá um reajuste em todos os preços dos filmes, com base em sua duração. Para todos os filmes com duração entre 0 e 100, o valor do aluguel passará a ser $10,00, e o aluguel dos filmes com duração acima de 100 passará a ser de $20,00.
