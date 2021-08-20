209d20625c79073947f4bf45fc811ae117b85cb5
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-20 15:06:14 -0300

    Exercise fixation 2, 20.4, req 4, finish

--- bloco 20/20.4/exercise-fixation-2-20-4.sql
+++ bloco 20/20.4/exercise-fixation-2-20-4.sql
@@ -14,3 +14,9 @@ WHERE length > 100
 AND rating IN('G', 'PG', 'PG-13')
 AND replacement_cost > 20;
 -- Foi determinado pelo setor financeiro que haverá um reajuste em todos os preços dos filmes, com base em sua duração. Para todos os filmes com duração entre 0 e 100, o valor do aluguel passará a ser $10,00, e o aluguel dos filmes com duração acima de 100 passará a ser de $20,00.
+UPDATE sakila.film
+SET rental_rate = (
+CASE 
+WHEN length BETWEEN 0 AND 100 THEN 10 
+WHEN length > 100 THEN 20
+END);
