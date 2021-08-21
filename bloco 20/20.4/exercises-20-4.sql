6f202efdf10d3a50f00005956a6d0f7b90a7b2ec
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-21 13:54:48 -0300

    Exercise 20.4, req 2

--- bloco 20/20.4/exercises-20-4.sql
+++ bloco 20/20.4/exercises-20-4.sql
@@ -9,6 +9,8 @@ VALUES ('Monstros SA', 'Pete Docter', '2001', 92),
 ('Os Incríveis', 'Brad Bird', '2004', 116),
 ('WALL-E', 'Pete Docter', 2008, 104);
 -- Exercício 2 : Procurando Nemo foi aclamado pela crítica! Foi classificado em 6.8, fez 450 milhões no mercado interno e 370 milhões no mercado internacional. Adicione as informações à tabela BoxOffice .
+INSERT INTO pixar.boxoffice(movie_id, rating, domestic_sales, international_sales)
+VALUES(9, 6.8, 450000000, 370000000);
 -- Exercício 3 : O diretor do filme "Procurando Nemo" está incorreto, na verdade ele foi dirigido por Andrew Staton. Corrija esse dado utilizando o UPDATE .
 -- Exercício 4 : O título do filme "Ratatouille" esta escrito de forma incorreta na tabela Movies , além disso, o filme foi lançado em 2007 e não em 2010. Corrija esses dados utilizando o UPDATE .
 -- Exercício 5 : Insira as novas classificações abaixo na tabela BoxOffice , lembre-se que a coluna movie_id é uma foreign key referente a coluna id da tabela Movies :
