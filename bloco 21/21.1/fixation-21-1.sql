7248ee7e207de7546b333d03755282910bce29a7
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-24 11:27:37 -0300

    Exercise fixation, 21.1, req 4

--- bloco 21/21.1/fixation-21-1.sql
+++ bloco 21/21.1/fixation-21-1.sql
@@ -5,4 +5,5 @@ SELECT REPLACE('Você já ouviu falar do DuckDuckGo ?', 'DuckDuckGo', 'Google');
 -- Utilizando uma query , encontre quantos caracteres temos em 'Uma frase qualquer' .
 SELECT LENGTH('Uma frase qualquer');
 -- Extraia e retorne a palavra "JavaScript" da frase 'A linguagem JavaScript está entre as mais usadas' .
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 12, 11);
 -- Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa
