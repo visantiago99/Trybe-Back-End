e1b3996170d201e797a243f76585c206de07a119
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-24 16:02:17 -0300

    Exercise fixation 4, 21.1, req 2

--- bloco 21/21.1/fixation4-21-1.sql
+++ bloco 21/21.1/fixation4-21-1.sql
@@ -2,5 +2,7 @@
 SELECT active, COUNT(*) FROM sakila.customer
 GROUP BY active;
 -- Monte uma query para a tabela sakila.customer que exiba a quantidade de clientes ativos e inativos por loja. Os resultados devem conter o ID da loja , o status dos clientes (ativos ou inativos) e a quantidade de clientes por status .
+SELECT store_id, active, COUNT(active) FROM sakila.customer
+GROUP BY store_id, active;
 -- Monte uma query que exiba a média de duração de locação por classificação indicativa ( rating ) dos filmes cadastrados na tabela sakila.film . Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor.
 -- Monte uma query para a tabela sakila.address que exiba o nome do distrito e a quantidade de endereços registrados nele. Os resultados devem ser ordenados da maior quantidade para a menor.
