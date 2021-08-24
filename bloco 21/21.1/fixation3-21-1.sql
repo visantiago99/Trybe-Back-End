dfd5b3360ee4423bddcd49aa8961f925684ff3da
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-24 15:41:06 -0300

    Exercise fixation 3, 21.1, req 8

--- bloco 21/21.1/fixation3-21-1.sql
+++ bloco 21/21.1/fixation3-21-1.sql
@@ -12,3 +12,16 @@ SELECT ROUND(15.7515971);
 SELECT FLOOR(39.494);
 -- Temos uma taxa de inscrição de 85.234% no curso de fotografia para iniciantes. Qual é o valor aproximado para cima dessa média?
 SELECT CEIL(85.234);
+-- Monte um query que exiba:
+-- A média de duração dos filmes e dê o nome da coluna de 'Média de Duração';
+-- A duração mínima dos filmes como 'Duração Mínima';
+-- A duração máxima dos filmes como 'Duração Máxima';
+-- A soma de todas as durações como 'Tempo de Exibição Total';
+-- E finalmente, a quantidade total de filmes cadastrados na tabela sakila.film como 'Filmes Registrados'.
+SELECT AVG(length)
+AS 'Média de duração',
+MIN(length) AS 'Duração Mínima',
+MAX(length) AS 'Duração Máxima',
+SUM(length) AS 'Tempo de Exibição Total',
+COUNT(*) AS 'Filmes Registrados'
+FROM sakila.film;
