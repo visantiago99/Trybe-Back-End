ecb1231b8dda692cceba87fa366e03d32c8f0d0e
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-24 15:06:49 -0300

    Exercise fixation 3, 21.1, req 4

--- bloco 21/21.1/fixation3-21-1.sql
+++ bloco 21/21.1/fixation3-21-1.sql
@@ -4,3 +4,8 @@ SELECT IF(15 MOD 2 <> 0, 'Ímpar', 'Par') AS 'Par ou Ímpar';
 SELECT 220 DIV 12;
 -- Utilizando o resultado anterior, responda à seguinte pergunta: temos lugares sobrando? Se sim, quantos?
 SELECT 220 MOD 12;
+-- Monte uma query que gere um valor entre 15 e 20 .
+SELECT ROUND(15 + (RAND() * 5));
+-- Monte uma query que exiba o valor arredondado de 15.7515971 com uma precisão de 5 casas decimais.
+-- Estamos com uma média de 39.494 de vendas de camisas por mês. Qual é o valor aproximado para baixo dessa média?
+-- Temos uma taxa de inscrição de 85.234% no curso de fotografia para iniciantes. Qual é o valor aproximado para cima dessa média?
