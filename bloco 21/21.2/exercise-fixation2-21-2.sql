db24ac348f972a88c6089a88a85166f8649e43b6
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-25 15:37:36 -0300

    Exercise fixation 2, 21.2, req 3

--- bloco 21/21.2/exercise-fixation2-21-2.sql
+++ bloco 21/21.2/exercise-fixation2-21-2.sql
@@ -7,4 +7,10 @@ SELECT first_name FROM sakila.customer WHERE first_name LIKE '%tracy%'
 UNION
 SELECT first_name FROM sakila.actor WHERE first_name LIKE '%je%';
 -- Monte uma query que exiba a união dos cinco últimos nomes da tabela actor , o primeiro nome da tabela staff e cinco nomes a partir da 15ª posição da tabela customer . Não permita que dados repetidos sejam exibidos. Ordene os resultados em ordem alfabética.
+(SELECT first_name FROM sakila.actor LIMIT 5 OFFSET 195)
+UNION
+(SELECT first_name FROM sakila.staff LIMIT 1)
+UNION
+(SELECT first_name FROM sakila.customer LIMIT 5 OFFSET 15)
+ORDER BY first_name
 -- Você quer exibir uma lista paginada com os nomes e sobrenomes de todos os clientes e atores do banco de dados, em ordem alfabética. Considere que a paginação está sendo feita de 15 em 15 resultados e que você está na 4ª página. Monte uma query que simule esse cenário.
