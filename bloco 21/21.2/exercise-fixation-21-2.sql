2120cbe0b79f43de13ad3080cbde6c5e09e06b57
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-25 12:17:03 -0300

    Exercise fixation 1, 21.2, req 2

--- bloco 21/21.2/exercise-fixation-21-2.sql
+++ bloco 21/21.2/exercise-fixation-21-2.sql
@@ -4,6 +4,10 @@ FROM sakila.actor AS A
 INNER JOIN sakila.film_actor AS B
 ON A.actor_id = B.actor_id;
 -- Use o JOIN para exibir o nome , sobrenome e endereço de cada um dos funcionários do banco. Use as tabelas staff e address .
+SELECT A.first_name, A.last_name, B.address
+FROM sakila.staff AS A
+INNER JOIN sakila.address AS B
+ON A.address_id = B.address_id;
 -- Exiba o id do cliente , nome e email dos primeiros 100 clientes, ordenados pelo nome em ordem decrescente, juntamente com o id do endereço e o nome da rua onde o cliente mora. Essas informações podem ser encontradas nas tabelas customer e address .
 -- Exiba o nome , email , id do endereço , endereço e distrito dos clientes que moram no distrito da California e que contêm "rene" em seus nomes. As informações podem ser encontradas nas tabelas address e customer .
 -- Exiba o nome e a quantidade de endereços dos clientes cadastrados. Ordene seus resultados por nomes de forma decrescente. Exiba somente os clientes ativos. As informações podem ser encontradas na tabela address e customer .
