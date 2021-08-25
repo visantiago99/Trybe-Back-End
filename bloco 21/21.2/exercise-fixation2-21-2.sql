e3e7a4f95fe572d5a9f89659faff273ca34da2e3
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-25 15:45:59 -0300

    Exercise fixation 2, 21.2, req 4

--- bloco 21/21.2/exercise-fixation2-21-2.sql
+++ bloco 21/21.2/exercise-fixation2-21-2.sql
@@ -14,3 +14,7 @@ UNION
 (SELECT first_name FROM sakila.customer LIMIT 5 OFFSET 15)
 ORDER BY first_name
 -- Você quer exibir uma lista paginada com os nomes e sobrenomes de todos os clientes e atores do banco de dados, em ordem alfabética. Considere que a paginação está sendo feita de 15 em 15 resultados e que você está na 4ª página. Monte uma query que simule esse cenário.
+(SELECT first_name, last_name FROM sakila.actor ORDER BY first_name, last_name LIMIT 60)
+UNION ALL
+(SELECT first_name, last_name FROM sakila.customer ORDER BY first_name, last_name LIMIT 60)
+LIMIT 15 OFFSET 45;
