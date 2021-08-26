46faf759d09a498d6a46e3ec73f048403bd5cc27
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-26 12:14:57 -0300

    Exercise fixation, 21.3, req 2

--- bloco 21/21.3/exercise-fixation-21-3.sql
+++ bloco 21/21.3/exercise-fixation-21-3.sql
@@ -13,4 +13,17 @@ DELIMITER ;
 
 CALL ShowBestActors();
 -- Monte uma procedure que receba como parâmetro de entrada o nome da categoria desejada em uma string e que exiba o id do filme , seu titulo , o id de sua categoria e o nome da categoria selecionada. Use as tabelas film , film_category e category para montar essa procedure.
+USE sakila;
+DELIMITER $$
+CREATE PROCEDURE ShowMovies(IN categoria VARCHAR(50))
+BEGIN 
+SELECT A.film_id, A.title, B.category_id, C.name FROM sakila.film AS A
+INNER JOIN sakila.film_category AS B
+ON A.film_id = B.film_id
+INNER JOIN sakila.category AS C
+ON B.category_id = C.category_id;
+END $$
+DELIMITER ;
+
+CALL ShowMovies('action');
 -- Monte uma procedure que receba o email de um cliente como parâmetro de entrada e diga se o cliente está ou não ativo, através de um parâmetro de saída.
