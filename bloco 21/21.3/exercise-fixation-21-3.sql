dfbbda53c38cf06fc3d4d10c124d5c5d855abbef
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-26 13:35:20 -0300

    Exercise fixation, 21.3, req 5

--- bloco 21/21.3/exercise-fixation-21-3.sql
+++ bloco 21/21.3/exercise-fixation-21-3.sql
@@ -57,4 +57,24 @@ RETURN quantity;
 END $$
 DELIMITER ;
 -- Crie uma function que, dado o parâmetro de entrada inventory_id , retorna o nome do filme vinculado ao registro de inventário com esse id.
+USE sakila;
+DELIMITER $$
+
+CREATE FUNCTION GetCorrespondingFilm(id INT)
+RETURNS VARCHAR(500) READS SQL DATA
+BEGIN
+    DECLARE movie_title VARCHAR(500);
+    SELECT distinct F.title
+    FROM sakila.inventory I
+    INNER JOIN sakila.film F
+    ON F.film_id = I.film_id
+    WHERE I.inventory_id = id INTO movie_title;
+    RETURN movie_title;
+END $$
+
+DELIMITER ;
+
+-- Como usar:
+
+SELECT GetCorrespondingFilm(2);
 -- Crie uma function que receba uma determinada categoria de filme em formato de texto (ex: 'Action' , 'Horror' ) e retorna a quantidade total de filmes registrados nessa categoria.
