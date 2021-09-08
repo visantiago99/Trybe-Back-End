6d186c0d2be83c466ee848d9b20de28ceadbbba0
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-08 13:06:53 -0300

    Exercise back end MongoDB, 23.1, req 3

--- bloco 23/23.1/exercise-23-1.js
+++ bloco 23/23.1/exercise-23-1.js
@@ -3,6 +3,7 @@ db.bios.findOne({_id: 8});
 // Exercício 2 : Retorne o documento com o _id igual a 8, mas só exiba os atributos: _id e name .
 db.bios.findOne({_id: 8}, {name: 1});
 // Exercício 3 : Retorne apenas os atributos name e birth do documento com o _id igual a 8.
+db.bios.findOne({_id: 8}, {name: 1, birth: 1});
 // Exercício 4 : Retorne todos os documentos em que o atributo name.first seja igual a John , utilizando o método pretty() .
 // Exercício 5 : Retorne os 3 primeiros documentos da coleção bios utilizando o método pretty() .
 // Exercício 6 : Retorne 2 documentos da coleção bios pulando os 5 primeiros documentos.
