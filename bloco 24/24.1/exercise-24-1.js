c92303962d01f237999abc42b0d2812e2768c2b8
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-14 13:59:28 -0300

    Exercise 24.1, MongoDB, req 11

--- bloco 24/24.1/exercise-24-1.js
+++ bloco 24/24.1/exercise-24-1.js
@@ -53,5 +53,9 @@ db.class.updateOne(
   },
 );
 // Exercício 11 : Utilizando uma única operação, crie um campo chamado sequels e atribua a ele o valor 0 em todos os documentos.
+db.class.updateMany(
+  {},
+  {$set: {sequels: 0}}
+  );
 // Exercício 12 : Utilizando uma única operação, remova os campos budget e estimatedBudget em todos os documentos.
 // Exercício 13 : Para os filmes Batman ou Home Alone , atribua a imdbRating o valor 17 , caso o valor de imdbRating seja menor que 17 .
