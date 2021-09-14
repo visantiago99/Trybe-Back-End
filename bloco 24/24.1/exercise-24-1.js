2c3531a2d56f882c685caeef32314ad477c7e7ea
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-14 14:03:23 -0300

    Exercise 24.1, MongoDB, req 13

--- bloco 24/24.1/exercise-24-1.js
+++ bloco 24/24.1/exercise-24-1.js
@@ -63,3 +63,16 @@ db.class.updateMany(
   {$unset: {budget: "", estimatedBudget: ""}}
   );
 // Exercício 13 : Para os filmes Batman ou Home Alone , atribua a imdbRating o valor 17 , caso o valor de imdbRating seja menor que 17 .
+db.class.updateMany(
+  {
+$or: [
+      { title: "Batman" },
+      { title: "Home Alone" },
+    ],
+  },
+  {
+$max: {
+      imdbRating: 17,
+    },
+  },
+);
