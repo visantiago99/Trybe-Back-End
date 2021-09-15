a1019d1d6edc7ea2e726c702e61d8d6f23d0e1ff
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-15 11:17:39 -0300

    Exercise 24.2, MongoDB, req 3

--- bloco 24/24.2/exercise-24-2.js
+++ bloco 24/24.2/exercise-24-2.js
@@ -11,6 +11,10 @@ db.class.updateOne(
   }}}
   );
 // Exercício 3: Remova a categoria "action" do filme Batman .
+db.class.updateOne(
+  {title: "Batman"},
+  {$pull: {category: "action"}}
+  );
 // Exercício 4: Remova o primeiro elemento do array category do filme Batman .
 // Exercício 5: Remova o último elemento do array category do filme Batman .
 // Exercício 6: Adicione o elemento "action" ao array category do filme Batman , garantindo que esse valor não se duplique.
