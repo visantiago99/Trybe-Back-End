98b042decc474cde7d8c0d7392780bcac7883299
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-15 12:24:28 -0300

    Exercise 24.2, MongoDB

--- bloco 24/24.2/exercise-24-2.js
+++ bloco 24/24.2/exercise-24-2.js
@@ -31,11 +31,90 @@ db.class.updateOne(
   {$addToSet: {category: "action"}}
   );
 // Exercício 7: Adicione a categoria "90's" aos filmes Batman e Home Alone .
+db.class.updateOne(
+  {title: "Batman"},
+  {$addToSet: {category: "action"}}
+  );
 // Exercício 8: Crie um array de documentos chamado cast para o filme Home Alone com os seguintes dados:
+db.class.updateOne(
+  {title: "Home Alone"},
+  {$push: {cast: {$each: [
+    {
+  "actor": "Macaulay Culkin",
+  "character": "Kevin"
+},
+{
+  "actor": "Joe Pesci",
+  "character": "Harry"
+},
+{
+  "actor": "Daniel Stern"
+}
+    ]}}}
+  );
 // Exercício 9: Adicione o campo character com o valor Marv ao array de cast em que o campo actor seja igual a Daniel Stern no filme Home Alone .
+db.class.updateOne(
+  {title: "Home Alone", "cast.actor": "Daniel Stern"},
+  {$set: {"cast.$.character": "Marv"}}
+  );
 // Exercício 10: Crie um array de documentos chamado cast para o filme Batman com os seguintes dados:
+db.class.updateOne(
+  {title: "Batman"},
+  {$push: {cast: {$each: [
+    {
+  "character": "Batman"
+},
+{
+  "character": "Alfred"
+},
+{
+  "character": "Coringa"
+}
+    ]}}}
+  );
 // Exercício 11: Produza três querys para o filme Batman :
 // Adicione o campo actor , que deve ser um array com o valor Christian Bale , ao array de cast em que o campo character seja igual a Batman ;
+db.movies.updateOne(
+  {
+    title: "Batman",
+    "cast.character": "Batman",
+  },
+  {
+    $push: { "cast.$.actor": "Christian Bale" },
+  },
+);
 // Adicione o campo actor , que deve ser um array com o valor Michael Caine , ao array de cast em que o campo character seja igual a Alfred ;
+db.movies.updateOne(
+  {
+    title: "Batman",
+    "cast.character": "Alfred",
+  },
+  {
+    $push: { "cast.$.actor": "Michael Caine" },
+  },
+);
 // Adicione o campo actor , que deve ser um array com o valor Heath Ledger , ao array de cast em que o campo character seja igual a Coringa .
+db.movies.updateOne(
+  {
+    title: "Batman",
+    "cast.character": "Coringa",
+  },
+  {
+    $push: { "cast.$.actor": "Heath Ledger" },
+  },
+);
 // Exercício 12: Adicione aos atores de cast do character Batman do filme Batman os valores "Michael Keaton" , "Val Kilmer" e "George Clooney" , e deixe o array em ordem alfabética.
+db.movies.updateOne(
+  {
+    title: "Batman",
+    "cast.character": "Batman",
+  },
+  {
+    $push: {
+      "cast.$.actor": {
+        $each: ["Michael Keaton", "Val Kilmer", "George Clooney"],
+        $sort: 1,
+      },
+    },
+  },
+);
